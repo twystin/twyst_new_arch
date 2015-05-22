cff621f99a605ec69c2a4d1d06b7b0fee493a1c9
arunr <arunr@outlook.com>
2015-05-22 14:55:57 +0530

    Basic auth & authz

--- config/mustbe.cfg.js
+++ config/mustbe.cfg.js
@@ -1,22 +1,46 @@
 var mustBe = require("mustbe");
 module.exports = function(config) {
 
+  config.userIdentity(function(id){
+
+    // determine if this user is authenticated or not
+    id.isAuthenticated(function(user, cb){
+      // note that the "user" in this case, is the user
+      // that was supplied by the routeHelpers.getUser function
+      var isAuthenticated = false;
+      if (user) {
+        isAuthenticated = true;
+      }
+      cb(null, isAuthenticated);
+    });
+
+  });
+
   config.routeHelpers(function(rh) {
     // get the current user from the request object
     rh.getUser(function(req, cb) {
-      // return cb(err); if there is an error
-      cb(null, req.user);
+      if (req.query && req.query.token) {
+        cb(null, req.query.token);
+      } else {
+        cb(null)
+      }
+      // cb(null, req.user);
     });
 
     // what do we do when the user is not authorized?
     rh.notAuthorized(function(req, res, next) {
-      res.redirect("/login?msg=you are not authorized");
+      res.status(200).send({
+        "status": false,
+        "message": "not authorized"
+      });
     });
   });
 
   config.activities(function(activities) {
     // configure an activity with an authorization check
     activities.can("view thing", function(identity, params, cb) {
+      // identity.isAuthenticated();
+      console.log("IDENTITY" + JSON.stringify(identity));
       cb(null, '1');
     });
   });
