008e7ed76e8b7c22997c8489dfad106dadcc7555
arunr <arunr@outlook.com>
2015-06-06 21:46:39 +0200

    Updates to reco

--- controllers/reco.ctrl.js
+++ controllers/reco.ctrl.js
@@ -34,12 +34,12 @@ module.exports.get = function(req, res) {
       massaged_item.name = item.basics.name;
       massaged_item.city = item.contact.location.city;
       massaged_item.address = item.contact.location.address;
-      massaged_item.distance = 'TBD';
+      massaged_item.distance = distance(lat, long, item.contact.location.coords.latitude, item.contact.location.coords.longitude, 'K');
       massaged_item.phone = item.contact.phones.mobile[0] && item.contact.phones.mobile[0].num;
       massaged_item.open = 'TBD';
       massaged_item.thumbnail = item.photos.others[0] && item.photos.others[0].image._th;
       massaged_item.offers = 'TBD';
-      // console.log(massaged_item);
+
       return massaged_item;
     }
 
@@ -48,3 +48,25 @@ module.exports.get = function(req, res) {
     return massaged_data;
   }
 };
+
+
+function distance(lat1, lon1, lat2, lon2, unit) {
+	var radlat1 = Math.PI * lat1/180;
+	var radlat2 = Math.PI * lat2/180;
+	var radlon1 = Math.PI * lon1/180;
+	var radlon2 = Math.PI * lon2/180;
+	var theta = lon1-lon2;
+	var radtheta = Math.PI * theta/180;
+	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+	dist = Math.acos(dist);
+	dist = dist * 180/Math.PI;
+	dist = dist * 60 * 1.1515;
+	if (unit === 'K') {
+    dist = dist * 1.609344;
+  }
+	if (unit === 'N') {
+    dist = dist * 0.8684;
+  }
+
+	return dist;
+}
