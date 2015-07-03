ac246d39005e8c688cd550fc6f8cc8d731f0ece9
arunr <arunr@outlook.com>
2015-07-03 23:49:53 +0100

    Updated follow API

--- controllers/event.ctrl.js
+++ controllers/event.ctrl.js
@@ -14,18 +14,19 @@ var _ = require('underscore');
 var Q = require('q');
 
 function check_event(data) {
+    console.log("CHECK EVENT BEGIN");
     var deferred = Q.defer();
     var passed_data = data;
 
-    if (!passed_data.req.query.token) {
+    if (!passed_data.user_token) {
         deferred.reject("Authentication error - no token passed.");
     }
 
-    if (!passed_data.req.body.event_outlet) {
+    if (!passed_data.event_data.event_outlet) {
         deferred.reject('No outlet to log the event at - please pass event_outlet');
     }
 
-    if (!passed_data.req.body.event_type) {
+    if (!passed_data.event_data.event_type) {
         deferred.reject('No event type - please pass event_type');
     }
 
@@ -35,10 +36,12 @@ function check_event(data) {
 }
 
 function get_user(data) {
+    console.log("GET USER BEGIN");
+
     var deferred = Q.defer();
     var passed_data = data;
 
-    var token = data.req.query.token || null;
+    var token = passed_data.user_token || null;
 
     AuthHelper.get_user(token).then(function(data) {
         passed_data.user = data.data;
@@ -51,11 +54,12 @@ function get_user(data) {
 }
 
 function get_outlet(data) {
+    console.log("GET OUTLET BEGIN");
+
     var deferred = Q.defer();
     var event = {};
-    event = _.extend(event, data.req.body);
-
     var passed_data = data;
+    event = _.extend(event, passed_data.event_data);
 
     OutletHelper.get_outlet(event.event_outlet).then(function(data) {
         passed_data.outlet = data.data;
@@ -68,11 +72,13 @@ function get_outlet(data) {
 }
 
 function create_event(data) {
+    console.log("CREATE EVENT BEGIN");
+
     var deferred = Q.defer();
     var event = {};
-    event = _.extend(event, data.req.body);
-
     var passed_data = data;
+
+    event = _.extend(event, passed_data.event_data);
     event.event_user = passed_data.user._id;
     event.event_outlet = passed_data.outlet._id;
     event.event_date = new Date();
@@ -90,16 +96,18 @@ function create_event(data) {
 
 var follow_processor = {
     process: function(data) {
+        console.log("FOLLOW PROCESSOR BEGIN");
+
         var deferred = Q.defer();
         var passed_data = data;
-        var updated_user = data.user;
-        var token = data.req.query.token;
+        var updated_user = passed_data.user;
+        var token = passed_data.user_token;
+
         updated_user.following  = updated_user.following || [];
-        updated_user.following.push(data.req.body.event_outlet);
+        updated_user.following.push(passed_data.event_data.event_outlet);
         updated_user.following = _.uniq(updated_user.following, false, function(f) {
             return f.toString();
         });
-
         UserHelper.update_user(token, updated_user).then(function(data) {
             deferred.resolve(passed_data);
         }, function(err) {
@@ -117,7 +125,6 @@ var checkin_processor = {
     }
 };
 
-
 function event_processor(event_type) {
     var processors = {
         'follow': follow_processor,
@@ -128,10 +135,13 @@ function event_processor(event_type) {
 }
 
 function process_event(data) {
+    console.log("PROCESS EVENT BEGIN");
+
     var deferred = Q.defer();
     var passed_data = data;
 
-    var event_type = passed_data.req.body.event_type
+    var event_type = passed_data.event_data.event_type;
+
     var processor = event_processor(event_type);
 
     if (!processor) {
@@ -148,10 +158,7 @@ function process_event(data) {
     return deferred.promise;
 }
 
-
-module.exports.new = function(req, res) {
-    var passed_data = {};
-    passed_data.req = req;
+function create_new(res, passed_data) {
     check_event(passed_data)
         .then(function(data) {
             return get_user(data)
@@ -171,89 +178,31 @@ module.exports.new = function(req, res) {
         .fail(function(err) {
             HttpHelper.error(res, err || false, "Error processing the event");
         });
+}
 
+module.exports.new = function(req, res) {
+    var passed_data = {};
+
+    passed_data.event_data = req.body || {};
+    passed_data.user_token = req.query.token || null;
+    passed_data.query_params = req.params || null;
+    create_new(res, passed_data);
 };
 
-module.exports.new_old = function(req, res) {
-    var token = req.query.token || null;
-    var created_event = {};
-    created_event = _.extend(created_event, req.body);
-
-    create_event(res, token, created_event).then(function(data) {
-        HttpHelper.success(res, data.data, data.message);
-    }, function(err) {
-        HttpHelper.error(res, err.data, err.message);
-    });
-};
 
 module.exports.follow = function(req, res) {
-    var token = req.query.token || null;
-    var created_event = {};
-    created_event = _.extend(created_event, req.body);
-    created_event.event_type = 'favourite';
+    var passed_data = {};
 
-    create_event(res, token, created_event).then(function(data) {
-        HttpHelper.success(res, data.data, data.message);
-    }, function(err) {
-        HttpHelper.error(res, err.data, err.message);
-    });
+    passed_data.event_data = req.body || {};
+    passed_data.event_data.event_type = 'follow';
+    passed_data.user_token = req.query.token || null;
+    passed_data.query_params = req.params || null;
+
+    create_new(res, passed_data);
+
 };
 
 module.exports.unfollow = function(req, res) {
 
 };
 
-var create_event2 = function(res, token, created_event) {
-    var deferred = Q.defer();
-    var event = null;
-
-    if (!token) {
-        HttpHelper.error(res, null, "Not authenticated");
-    }
-
-    // CAN THERE BE NON OUTLET SPECIFIC EVENTS?
-    if (!created_event.event_outlet) {
-        HttpHelper.error(res, true, "No outlet specified");
-    } else {
-        Outlet.findOne({_id: created_event.event_outlet}, function(err, outlet) {
-           if (err || !outlet) {
-               HttpHelper.error(res, err || true, "Couldnt find the outlet");
-           } else {
-               AuthHelper.get_user(token).then(function(data) {
-                    var updated_user = data.data;
-                   created_event.event_user = updated_user;
-                   created_event.event_date = new Date()
-                   event = new Event(created_event);
-                   event.save(function(err, e) {
-                       if (err || !e) {
-                           deferred.reject({err: err || true, message: 'Couldn\'t save the event.'});
-                       } else {
-                           if (created_event.event_type === 'favourite') {
-                               // Now, update the user!
-                               updated_user.following  = updated_user.following || [];
-                               updated_user.following.push(created_event.event_outlet);
-                               console.log(updated_user.following);
-                               updated_user.following = _.uniq(updated_user.following, false, function(f) {
-                                   return f.toString();
-                               });
-                               console.log(updated_user.following);
-
-                               UserHelper.update_user(token, updated_user).then(function(data) {
-                                   deferred.resolve({data: e, message: 'Successfully created the event'});
-                               }, function(err) {
-                                   deferred.reject();
-                               });
-                           } else {
-                               deferred.resolve({data:'', message:'Successfully created the event'});
-                           }
-                       }
-                   });
-               }, function(err) {
-                   deferred.reject({err: err || true, message: 'Couldn\'t find the user'});
-               });
-
-           }
-        });
-    }
-    return deferred.promise;
-};
