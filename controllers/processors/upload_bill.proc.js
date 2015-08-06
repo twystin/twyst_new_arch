14b7c1c0001296a132dc1be219f58e083bc87667
Hemant Wadhwa <hemantwadhwa47@gmail.com>
2015-08-06 12:53:40 +0530

    Revert "replaced _.get with _.has wherever appropriate"
    This reverts commit 9292330634c1035f033c5b44fdfec0e38e0d6b7b.

--- controllers/processors/upload_bill.proc.js
+++ controllers/processors/upload_bill.proc.js
@@ -6,10 +6,10 @@ module.exports.check = function(data) {
   logger.log();
   var deferred = Q.defer();
   var passed_data = data;
-  var bill_date = _.has(passed_data, 'event_data.event_meta.bill_date');
-  var photo = _.has(passed_data, 'event_data.event_meta.photo');
-  var outlet_name = _.has(passed_data, 'event_data.event_meta.outlet_name');
-  var outlet = _.has(passed_data, 'event_data.event_outlet');
+  var bill_date = _.get(passed_data, 'event_data.event_meta.bill_date');
+  var photo = _.get(passed_data, 'event_data.event_meta.photo');
+  var outlet_name = _.get(passed_data, 'event_data.event_meta.outlet_name');
+  var outlet = _.get(passed_data, 'event_data.event_outlet');
 
   if (!bill_date || !photo || !outlet_name) {
     deferred.reject('Submit bill needs to have bill date, photo & outlet name.');
