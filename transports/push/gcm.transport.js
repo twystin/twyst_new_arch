24142e640a239bd435439271d5368487c45b64db
Arun Rajappa <ar@Aruns-MacBook-Air.local>
2015-08-17 19:16:21 +0530

    GCM Transport

---
+++ transports/push/gcm.transport.js
@@ -0,0 +1,33 @@
+'use strict';
+/*jslint node: true */
+
+var logger = require('tracer').colorConsole();
+var _ = require('lodash');
+var Q = require('q');
+
+var gcm = require('node-gcm');
+var fs = require('fs');
+
+module.exports.send = function(payload) {
+  logger.log();
+  var deferred = Q.defer();
+
+  var sender = new gcm.Sender(payload.server_key);
+  var message = new gcm.Message();
+  message.addData('message', payload.body);
+  message.addData('title', payload.head);
+  message.addData('msgcnt', '1'); 
+  message.addData('soundname', 'beep.wav'); 
+  message.timeToLive = 10000; 
+
+  var registrationIds = [];
+  registrationIds = registrationIds.concat(payload.gcms);
+
+  logger.log(message);
+  sender.send(message, registrationIds, 4, function(result) {
+    deferred.resolve(result);
+  });
+
+
+  return deferred.promise;
+}
