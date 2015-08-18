2583154fd1ddd1a5d1a997072123447622537173
Arun Rajappa <ar@Aruns-MacBook-Air.local>
2015-08-18 11:21:51 +0530

    Notification sample

--- notifications/coupon_unlocked.notfn.js
+++ notifications/coupon_unlocked.notfn.js
@@ -8,11 +8,11 @@ var Q = require('q');
 var Transporter = require('../transports/transporter');
 
 // THIS CAN GET CALLED FROM JOBS OR FROM CODE
-module.exports.notify = function(what, who, campaign) {
+module.exports.notify = function(what, when, who, campaign) {
   logger.log();
 
   var deferred = Q.defer();
-  var notify_text = "Your voucher for " + what.coupon.header + " at " + what.coupon.issued_by + " is now available";
+  var notify_text = "Your voucher for " + what.coupon.header + " at " + what.coupon.issued_by + " is now available.";
   var payload = {
   	server_key: "" //TBD,
   	body: notify_text,
@@ -20,11 +20,19 @@ module.exports.notify = function(what, who, campaign) {
   	gcms: who
   };
 
-  transporter.send("push", "gcm", payload).then(function(data) {
-  	deferred.resolve(data);
-  }, function(err) {
-  	deferred.reject(err);
-  });
+  if (when) {
+    transporter.schedule("push", "gcm", when, payload).then(function(data) {
+      deferred.resolve(data);
+    }, function(err) {
+      deferred.reject(err);
+    })
+  } else {
+    transporter.send("push", "gcm", payload).then(function(data) {
+      deferred.resolve(data);
+    }, function(err) {
+      deferred.reject(err);
+    });
+  }
 
   return deferred.promise;
 
