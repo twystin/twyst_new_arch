40f8d17eb32842c4c239f1cf86f3c5278280f606
Arun Rajappa <ar@Aruns-MacBook-Air.local>
2015-08-21 14:28:35 +0530

    Updated job_server

--- job_server.js
+++ job_server.js
@@ -1,2 +1,25 @@
-// THIS STARTS THE JOB SERVER
-require('./config/jobs.cfg').start();
+'use strict';
+/*jslint node: true */
+
+var Agenda = require('agenda');
+var _ = require('lodash');
+var logger = require('tracer').colorConsole();
+
+var jobs = [
+	// {name: 'test', schedule: 'in 1 minute'}
+	{name: 'unredeemed', schedule: 'in 2 minutes'}
+];
+
+(function() {
+	logger.info('Starting the job server');
+	var agenda = new Agenda({db: { address: 'localhost:27017/twyst_agenda'}});
+
+	var job = null;
+	_.each(jobs, function(item) {
+		job = require('./jobs/' + item.name + '.job');
+		job.runner(agenda);
+		agenda.schedule(item.schedule, item.name);
+	})
+
+	agenda.start();
+})();
