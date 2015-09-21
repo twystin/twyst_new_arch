4b43cad534ceeec054a8b50842dcdc7e448f05e0
Kuldeep Patel <kuldeepp89@gmail.com>
2015-09-21 13:24:02 +0530

    some more fixes

--- controllers/helpers/user.hlpr.js
+++ controllers/helpers/user.hlpr.js
@@ -19,41 +19,39 @@ module.exports.update_user = function(token, updated_user) {
   var deferred = Q.defer();
   AuthHelper.get_user(token).then(function(data) {
     var user = data.data;
-
+    console.log(updated_user);
     user = ld.merge(user, updated_user);
-
-    if (user.gcmId) {
-      var index = ld.findIndex(user.push_ids, function(push) {
-        return push.push_id == gcmId;
-      });
-      if (index === -1) {
-        var push_info = {
-          push_type: 'gcm',
-          push_id: user.gcmId,
-          push_meta: {}
-
-        };
-        user.push_ids.push(push_info);
-      }
+    
+    if(user.gcmId) {
+        var index = ld.findIndex(user.push_ids, function(push) { return push.push_id==gcmId; });
+        if(index==-1) {
+          var push_info = {
+              push_type: 'gcm',
+              push_id: user.gcmId,
+              push_meta: {} 
+              
+          };   
+          user.push_ids.push(push_info);
+        }
     }
-    if (user.lat && user.long) {
-      user.locations = {};
-      user.locations.coords = {};
-      user.locations.coords.lat = user.lat;
-      user.locations.coords.log = user.long;
-      user.locations.when = new Date();
+    if(user.lat && user.long) {
+        user.locations = {};
+        user.locations.coords ={};
+        user.locations.coords.lat = user.lat;
+        user.locations.coords.log = user.long;
+        user.locations.when = new Date();
     }
 
-    if (user.source) {
+    if(user.source) {
       user[updated_user.source.toLowerCase()] = updated_user;
     }
-
+    
     user.device_info = {
-      id: user.device,
-      os: user.os_version,
-      model: user.model
-    }
-    user.friends = user.friends._id;
+        id: user.device,
+        os: user.os_version,
+        model: user.model    
+    }  
+    user.friends = user.friends_id;
 
     delete user.os_version;
     delete user.model;
@@ -61,31 +59,25 @@ module.exports.update_user = function(token, updated_user) {
     delete user.lat;
     delete user.long;
     delete user.__v;
-    delete user.friends_id;
+    delete user.friends._id;
 
-    User.findOne({
+    User.findOneAndUpdate({
       _id: user._id
+    }, {
+      $set: user
+    }, {
+      upsert: true
     }).exec(function(err, u) {
-      if(err || !u) {
+      if (err || !u) {
+        console.log(err);
         deferred.reject({
-          err: err || false,
-          message: "Couldn't update the user"
+          err: err || true,
+          message: "Couldn\'t update user"
         });
       } else {
-        u = ld.merge(u, user);
-        u.save(function(err, user) {
-          if (err) {
-            logger.error(err);
-            deferred.reject({
-              err: err || true,
-              message: "Couldn\'t update user"
-            });
-          } else {
-            deferred.resolve({
-              data: user,
-              message: 'Updated user'
-            });
-          }
+        deferred.resolve({
+          data: u,
+          message: 'Updated user'
         });
       }
     });
@@ -100,112 +92,105 @@ module.exports.update_user = function(token, updated_user) {
 };
 
 module.exports.update_friends = function(token, friend_list) {
-  logger.log();
-  var deferred = Q.defer();
-  AuthHelper.get_user(token).then(function(data) {
-    var user = data.data;
-    var update_query = {
-      $pushAll: {
-        friends: []
-      }
-    };
-    async.each(friend_list.list, function(friend, callback) {
-      var index;
-
-      if (friend_list.source === 'GOOGLE' || friend_list.source === 'FACEBOOK') {
-        index = _.findIndex(user.friends, function(existing_friend) {
-          return existing_friend.social_id === friend.id;
-        });
-      } else {
-        index = _.findIndex(user.friends, function(existing_friend) {
-          return existing_friend.phone === friend.phone;
-        });
-      }
-      if (index === -1) {
-        var friend_obj = {
-          source: friend_list.source,
-          add_date: new Date(),
-          phone: '',
-          social_id: '',
-          name: friend.name,
-          email: friend.email
-        };
-        var user_obj = {
-          source: friend_list.source,
-          add_date: new Date(),
-          phone: user.phone,
-          social_id: '',
-          email: user.email,
-          user: user._id,
-          name: user.first_name
-        };
-
-        if (friend_list.source === 'GOOGLE' || friend_list.source === 'FACEBOOK') {
-          friend_obj.social_id = friend.id;
-        }
-        findFriend(friend.id).then(function(app_friend) {
-          if (app_friend) {
-            friend_obj.user = app_friend.id;
-            friend_obj.email = app_friend.email;
-            friend_obj.phone = app_friend.phone;
-            addUserReferral(user_obj, app_friend.friends);
-          }
-          update_query['$pushAll'].friends.push(friend_obj);
-          callback();
-        });
-      } else {
-        callback();
-      }
-    }, function() {
-      Friend.findOneAndUpdate({
-        '_id': user.friends_id
-      }, update_query, function(err, u) {
-        if (err || !u) {
-          deferred.reject({
-            err: err || true,
-            message: "Couldn\'t update user"
-          });
-        } else {
-          var friends = [];
-          var twyst_friends = [];
-          _.each(u.friends, function(friend) {
-            if (friend.user) {
-              twyst_friends.push(friend)
+    logger.log();
+    var deferred = Q.defer();
+    AuthHelper.get_user(token).then(function(data) {
+        var user = data.data;
+        var update_query = { $pushAll: { friends: [] } };
+        async.each(friend_list.list, function(friend, callback) {
+            var index;
+            if (friend_list.source === 'GOOGLE' || friend_list.source === 'FACEBOOK') {
+                index = _.findIndex(user.friends, function(existing_friend) { return existing_friend.social_id == friend.id; });
+            } 
+            else {
+                index = _.findIndex(user.friends, function(existing_friend) { return existing_friend.phone == friend.phone; });
             }
-            friends.push(friend);
-          })
-          user.twyst_friends = twyst_friends;
-          user.friends = friends;
-          deferred.resolve({
-            data: user,
-            message: 'Updated user'
-          });
+            if (index === -1) {
+                var friend_obj = { 
+                    source: friend_list.source, 
+                    add_date: new Date(), 
+                    phone: '', 
+                    social_id: '', 
+                    name: friend.name, 
+                    email: friend.email 
+                };
+
+                var user_obj = { 
+                    source: friend_list.source, 
+                    add_date: new Date(),
+                    phone: user.phone, 
+                    social_id: '',
+                    email: user.email, 
+                    user: user._id, 
+                    name: user.first_name 
+                };
+        
+                if (friend_list.source === 'GOOGLE' || friend_list.source === 'FACEBOOK') {
+                    friend_obj.social_id = friend.id;
+
+                    findFriendBySourceId(friend.id).then(function(app_friend) {
+                        if (app_friend) {
+                            friend_obj.user = app_friend.id;
+                            friend_obj.email = app_friend.email;
+                            friend_obj.phone = app_friend.phone;
+                            addUserReferral(user_obj, app_friend.friends);
+                        }
+                        update_query['$pushAll'].friends.push(friend_obj);
+                        callback();
+                    })
+
+                }
+                else {
+                    friend_obj.phone = friend.phone;
+
+                    findFriendBySourceId(friend.phone).then(function(app_friend) {
+                        if (app_friend) {
+                            friend_obj.user = app_friend.id;
+                            friend_obj.email = app_friend.email;
+                            addUserReferral(user_obj, app_friend.friends);
+                        }
+                        update_query['$pushAll'].friends.push(friend_obj);
+                        callback();
+                    })
+
+                }
+            } 
+            else {
+                callback();
+            }
+        }, 
+        function() {
+            console.log(update_query)
+            Friend.findOneAndUpdate({'_id': user.friends_id}, update_query, function(err, u) {
+                if (err || !u) {
+                    deferred.reject({
+                        err: err || true,
+                        message: "Couldn\'t update user"
+                    });
+                } 
+                else {
+                   // console.log(u)
+                    deferred.resolve({
+                        data: u,
+                        message: 'Updated user'
+                    });
+                }
+            });
         }
-      });
-    })
-
-  }, function(err) {
-    deferred.reject({
-      err: err || true,
-      message: "Couldn\'t find user"
+    )}, function(err) {
+        deferred.reject({
+          err: err || true,
+          message: "Couldn\'t find user"
+        });
     });
-  });
 
-  return deferred.promise;
+    return deferred.promise;
 };
 
-function findFriend(identity) {
+function findFriendBySourceId(friendId) {
   logger.log();
   var deferred = Q.defer();
-  User.findOne({
-    $or: [{
-      'facebook.id': identity
-    }, {
-      'google.id': identity
-    }, {
-      phone: phone
-    }]
-  }).exec(function(err, friend) {
+  User.findOne({ $or: [{ 'facebook.id': friendId }, { 'google.id': friendId }, {phone: friendId}] }).exec(function(err, friend) {
     if (err || !friend) {
       deferred.resolve();
     } else {
@@ -220,27 +205,32 @@ function findFriend(identity) {
   return deferred.promise;
 }
 
+
+
 function addUserReferral(user_obj, friendObjId) {
-  logger.log();
-  Friend.findOne({
-    '_id': ObjectId(friendObjId)
-  }).exec(function(err, obj) {
-    if (err || !obj) {} else {
-      var index = _.findIndex(obj.friends, function(friend) {
-        return friend.phone == user_obj.phone;
-      });
-      if (index == -1) {
-        obj.friends.push(user_obj);
-      } else {
-        obj.friends[index].user = user_obj.user.toString();
-      }
-      obj.save(function(err) {
-        if (err) {
-          logger.error(err);
-        } else {
-          logger.log('referral added successfully');
+    logger.log();
+    Friend.findOne({'_id': ObjectId(friendObjId)}).exec(function(err, obj) {
+        if(err || !obj) {
+
         }
-      });
-    }
-  });
+        else {
+            var index = _.findIndex(obj.friends, function(friend) { 
+                return friend.phone == user_obj.phone; 
+            });
+            if(index == -1) {
+                obj.friends.push(user_obj);
+            } 
+            else {
+                obj.friends[index].user = user_obj.user.toString();
+            }
+            obj.save(function(err) { 
+                if(err) { 
+                    logger.error(err); 
+                } 
+                else { 
+                    logger.log('referral added successfully'); 
+                }
+            });
+        }
+    });
 }
--- controllers/processors/grab.proc.js
+++ controllers/processors/grab.proc.js
@@ -15,70 +15,69 @@ module.exports.check = function(data) {
 };
 
 module.exports.process = function(data) {
-  logger.log();
-  var deferred = Q.defer();
+    logger.log();
+    var deferred = Q.defer();
 
-  var user_id = data.user._id;
-  var coupon_id = data.coupon._id;
-  var outlet_id = data.outlet._id;
-  var coupon = data.coupon;
-  //remove coupon from cache
-  _.each(coupon.social_friend_list, function(friend){
-    Cache.hget(friend, 'social_pool_coupons', function(err, reply){
-      if(err || !reply) {
+    var user_id = data.user._id;
+    var coupon_id = data.coupon._id;
+    var outlet_id = data.outlet._id;
+    var coupon = data.coupon;
+    //remove coupon from cache
+    _.each(coupon.social_friend_list, function(friend){
+        Cache.hget(friend, 'social_pool_coupons', function(err, reply){
+            if(err || !reply) {
 
-      }
-      else {
-        var new_social_pool = []
-        _.each(reply, function(coupon_in_social_pool){
-            if(coupon_in_social_pool._id != coupon._id) {
-                new_social_pool.push(coupon_in_social_pool);
+            }
+            else {
+                var new_social_pool = []
+                _.each(reply, function(coupon_in_social_pool){
+                    if(coupon_in_social_pool._id != coupon._id) {
+                        new_social_pool.push(coupon_in_social_pool);
+                    }
+                })
+                Cache.hset(friend, 'social_pool_coupons', new_social_pool);
             }
         })
-        Cache.hset(friend, 'social_pool_coupons', new_social_pool);
-      }
     })
-  };
-  coupon.is_grabed = true;
-  coupons.status = 'active';
-  var update = {
-    $push: {
-      coupons: {
-        coupon
-      }
+  
+    coupon.is_grabed = true;
+    coupons.status = 'active';
+    var update = {
+        $push: {
+            coupons: coupon
+        }
     }
     //update user who grabbed coupon
-    User.findOneAndUpdate({
-        '_id': user_id
-      }, update, function(err, u) {
+    User.findOneAndUpdate({'_id': user_id}, update, function(err, u) {
         if (err || !u) {
           deferred.reject({
             err: err || true,
             message: "Couldn\'t update user"
           });
-        } else {//update user whose coupon is being grabbed
-          User.findOneAndUpdate({
-            '_id': lapsed_coupon_source, coupons: {$elemMatch: {  _id: coupon._id }}
-          }, {$set: {'coupons.$.grabbed_by': user_id, 'coupons.$.status': 'grabbed'},
-           function(err, u) {
-            if (err || !u) {
-              deferred.reject({
-                err: err || true,
-                message: "Couldn\'t grab coupon"
-              });
-            }
-            else{
-                deferred.resolve({
-                    data: user,
-                    message: 'coupon grabbed successfully'
-                  });
-            }
-          
-          
+        } 
+        else {//update user whose coupon is being grabbed
+            User.findOneAndUpdate({'_id': lapsed_coupon_source,
+                coupons: {$elemMatch: {  _id: coupon._id }}
+                }, {$set: {'coupons.$.grabbed_by': user_id, 'coupons.$.status': 'grabbed'}},
+                function(err, u) {
+                    if (err || !u) {
+                        deferred.reject({
+                            err: err || true,
+                            message: "Couldn\'t grab coupon"
+                        });
+                    }
+                    else{
+                        deferred.resolve({
+                            data: user,
+                            message: 'coupon grabbed successfully'
+                        });
+                    }
+                }
+            )
         }
-      });
-  })
+      
+    })
 
-  deferred.resolve(true);
-  return deferred.promise;
+    deferred.resolve(true);
+    return deferred.promise;
 };
