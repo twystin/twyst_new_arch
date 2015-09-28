6d03c0b12c0db7e72095b660670f53f5d601a51e
Hemant Wadhwa <hemantwadhwa47@gmail.com>
2015-09-29 00:31:01 +0530

    merchant default view redirect

--- www/merchant/controllers/base.ctrl.js
+++ www/merchant/controllers/base.ctrl.js
@@ -1,8 +1,14 @@
 angular.module('merchantApp')
-	.controller('BaseController', ['$scope', '$cookies', '$log', '$state', 'toastr', '$rootScope', 'merchantRESTSvc',
-		function($scope, $cookies, $log, $state, $toastr, $rootScope, merchantRESTSvc) {
+	.controller('BaseController', ['$scope', '$cookies', '$log', '$state', 'toastr', '$rootScope', 'merchantRESTSvc', '$stateParams', 
+		function($scope, $cookies, $log, $state, $toastr, $rootScope, merchantRESTSvc, $stateParams) {
 
 			_id = undefined;
+			if($state.current.name==="merchant.default") {
+				$state.go('merchant.panel', {}, {
+					reload: true
+				});
+			}
+
 			
 		}
 	])
