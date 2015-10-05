02667ae285a5a2662d2173be283d32f617d7096e
Hemant Wadhwa <hemantwadhwa47@gmail.com>
2015-10-05 13:25:29 +0530

    icons added and merchant panel checkin date picker fix

--- controllers/processors/panel_checkin.proc.js
+++ controllers/processors/panel_checkin.proc.js
@@ -377,6 +377,7 @@ function send_sms(data) {
     payload.message = data.message;
     payload.phone = data.event_data.event_meta.phone;
     Transporter.send('sms', 'vf', payload);
+    deferred.resolve(data);
     return deferred.promise;
 }
 
