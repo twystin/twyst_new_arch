13359b909afde0dd47fe113ce02ee7fc860a094c
Hemant Wadhwa <hemantwadhwa47@gmail.com>
2015-11-27 18:14:15 +0530

    menu temp fix (creation working)

--- www/merchant/controllers/menu.create.ctrl.js
+++ www/merchant/controllers/menu.create.ctrl.js
@@ -2,16 +2,16 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 	function($scope, merchantRESTSvc, toastr, WizardHandler, $timeout, $state, $q, $modal) {
 		$scope.menu = {
 			status: 'active',
-			menu_description: []
+			menu_categories: []
 		};
 
-		// $scope.menu = {"status":"draft","menu_description":[{"sections":[{"items":[{"item_options":[{"add_on":[{"add_on_item":"sfvss","add_on_item_cost":"342"}],"option":"fvr","option_cost":546}],"item_name":"wd","item_description":"df","item_tags":["sdf","fre","yt"],"item_cost":345}],"section_name":"asd","section_description":"ads"}],"menu_category":"asd"}],"menu_type":"asd", "outlet": "556568e81ade70eb1974b956"};
+		// $scope.menu = {"status":"draft","menu_categories":[{"sections":[{"items":[{"item_options":[{"add_on":[{"add_on_item":"sfvss","add_on_item_cost":"342"}],"option":"fvr","option_cost":546}],"item_name":"wd","item_description":"df","item_tags":["sdf","fre","yt"],"item_cost":345}],"section_name":"asd","section_description":"ads"}],"menu_category":"asd"}],"menu_type":"asd", "outlet": "556568e81ade70eb1974b956"};
 
 		$scope.menu_types = ['Dine-In', 'Takeaway', 'Delivery', 'Weekend', 'Dinner', 'All'];
 		$scope.menu_categories = ['Indian', 'Desserts', 'Cakes', 'Chinese', 'Soup'];
 		$scope.section_names = ['Veg Starters', 'Non Veg Starters', 'Veg Main Course', 'Non Veg Main Course'];
 
-		$scope.addDesc = function() {
+		$scope.addMenuCategory = function() {
 			var modalInstance = $modal.open({
 				animation: true,
 				templateUrl: 'menuCategoryTemplate.html',
@@ -20,7 +20,7 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 				resolve: {
 					menu_category: function() {
 						return {
-							sections: [{section_name: 'Default', items: []}]
+							sub_categories: [{sub_category_name: 'Default', items: []}]
 						};
 					},
 					is_new: function() {
@@ -30,7 +30,7 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 			});
 
 			modalInstance.result.then(function(category) {
-				$scope.menu.menu_description.push(category);
+				$scope.menu.menu_categories.push(category);
 			}, function() {
 				console.log('Modal dismissed at: ', new Date());
 			});
@@ -44,7 +44,7 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 		})
 
 		$scope.manageDesc = function(index) {
-			if(!$scope.menu.menu_description || !$scope.menu.menu_description[index]) {
+			if(!$scope.menu.menu_categories || !$scope.menu.menu_categories[index]) {
 				toastr.error("Menu category out of bounds");
 			} else {
 				$scope.descIndex = index;
@@ -55,7 +55,7 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 					size: 'lg',
 					resolve: {
 						menu_category: function() {
-							return _.clone($scope.menu.menu_description[index] || {});
+							return _.clone($scope.menu.menu_categories[index] || {});
 						},
 						is_new: function() {
 							return false;
@@ -64,7 +64,7 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 				});
 
 				modalInstance.result.then(function(category) {
-					$scope.menu.menu_description[$scope.descIndex] = category;
+					$scope.menu.menu_categories[$scope.descIndex] = category;
 				}, function() {
 					console.log('Modal dismissed at: ', new Date());
 				});
@@ -73,18 +73,18 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 
 		$scope.removeDesc = function(index) {
 			if(confirm('Are you sure?')) {
-				$scope.menu.menu_description.splice(index, 1);
+				$scope.menu.menu_categories.splice(index, 1);
 			}
 		}
 
-		$scope.addSection = function(index) {
+		$scope.addSubCategory = function(index) {
 			var modalInstance = $modal.open({
 				animation: true,
-				templateUrl: 'menuSectionTemplate.html',
+				templateUrl: 'subCategoryTemplate.html',
 				controller: 'MenuSectionController',
 				size: 'lg',
 				resolve: {
-					section: function() {
+					sub_category: function() {
 						return {
 							items: []
 						};
@@ -96,7 +96,7 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 			});
 
 			modalInstance.result.then(function(section) {
-				$scope.menu.menu_description[index].sections.push(section);
+				$scope.menu.menu_categories[index].sub_categories.push(section);
 			}, function() {
 				console.log('Modal dismissed at: ', new Date());
 			})
@@ -108,11 +108,11 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 			} else {
 				var modalInstance = $modal.open({
 					animation: true,
-					templateUrl: 'menuSectionTemplate.html',
+					templateUrl: 'subCategoryTemplate.html',
 					controller: 'MenuSectionController',
 					size: 'lg',
 					resolve: {
-						section: function() {
+						sub_category: function() {
 							return _.clone(category.sections[index] || {});
 						},
 						is_new: function() {
@@ -147,7 +147,7 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 				size: 'lg',
 				resolve: {
 					item: function() {
-						return {item_options: []};
+						return {option_sets: []};
 					},
 					is_new: function() {
 						return true;
@@ -173,7 +173,7 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 					size: 'lg',
 					resolve: {
 						item: function() {
-							return _.clone(section.items[index] || {item_options: []})
+							return _.clone(section.items[index] || {option_sets: []})
 						},
 						is_new: function() {
 							return false
@@ -200,17 +200,17 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 		}
 
 		$scope.addItemOption = function(index) {
-			$scope.current_item.item_options.push({add_on: []});
+			$scope.current_item.option_sets.push({add_on: []});
 		}
 
 		$scope.removeItemOption = function(index) {
 			if(confirm('Are you sure?')) {
-				$scope.current_item.item_options.splice(index, 1);
+				$scope.current_item.option_sets.splice(index, 1);
 			}
 		}
 
 		$scope.addAddon = function(index) {
-			$scope.current_item.item_options[index].add_on.push({});
+			$scope.current_item.option_sets[index].add_on.push({});
 		}
 
 		$scope.removeAddon = function(list, index) {
@@ -223,7 +223,7 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 			$scope.validateItem().then(function() {
 				var item_obj = angular.copy($scope.current_item);
 				delete $scope.current_item;
-				$scope.menu.menu_description[$scope.descIndex].sections[$scope.sectionIndex].items.push(item_obj);
+				$scope.menu.menu_categories[$scope.descIndex].sections[$scope.sectionIndex].items.push(item_obj);
 				WizardHandler.wizard().goTo('Manage Section');
 			}, function(err) {
 				toastr.error(err, 'ERROR');
@@ -234,7 +234,7 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 			$scope.validateItem().then(function() {
 				var item_obj = angular.copy($scope.current_item);
 				delete $scope.current_item;
-				$scope.menu.menu_description[$scope.descIndex].sections[$scope.sectionIndex].items[$scope.itemIndex] = item_obj;
+				$scope.menu.menu_categories[$scope.descIndex].sections[$scope.sectionIndex].items[$scope.itemIndex] = item_obj;
 				WizardHandler.wizard().goTo('Manage Section');
 			}, function(err) {
 				toastr.error(err, 'ERROR');
@@ -244,9 +244,9 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 		$scope.addAnotherItem = function() {
 			var item_obj = angular.copy($scope.current_item);
 			$scope.current_item = {
-				item_options: []
+				option_sets: []
 			};
-			$scope.menu.menu_description[$scope.descIndex].sections[$scope.sectionIndex].items.push(item_obj);
+			$scope.menu.menu_categories[$scope.descIndex].sections[$scope.sectionIndex].items.push(item_obj);
 		}
 
 		$scope.cancelItem = function() {
@@ -255,13 +255,13 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 		}
 
 		$scope.backToDesc = function() {
-			async.each($scope.menu.menu_description, function(description, callback) {
-				if(!description.menu_category) {
+			async.each($scope.menu.menu_categories, function(categorycategory, callback) {
+				if(!categorycategory.menu_category) {
 					callback('Menu category name required');
-				} else if (!description.sections || !description.sections.length) {
+				} else if (!categorycategory.sections || !categorycategory.sections.length) {
 					callback('All menu categories must have atleast one section');
 				} else {
-					async.each(description.sections, function(section) {
+					async.each(categorycategory.sections, function(section) {
 						if (!section.section_name) {
 							callback('All sections must have a section name');
 						} else if (!section.items || !section.items.length) {
@@ -284,7 +284,7 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 		}
 
 		$scope.backToSection = function() {
-			if ($scope.menu.menu_description[$scope.descIndex].sections[$scope.sectionIndex].items.length === 0) {
+			if ($scope.menu.menu_categories[$scope.descIndex].sections[$scope.sectionIndex].items.length === 0) {
 				toastr.error('Atleast one item required in every section');
 			} else {
 				WizardHandler.wizard().goTo('Manage Desc');	
@@ -298,16 +298,16 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 			} else if (!$scope.menu.outlet) {
 				deferred.reject('Outlet id required');
 			} else {
-				async.each($scope.menu.menu_description, function(description, callback) {
-					if(!description.menu_category) {
+				async.each($scope.menu.menu_categories, function(category, callback) {
+					if(!category.category_name) {
 						callback('Menu category name required');
-					} else if (!description.sections || !description.sections.length) {
+					} else if (!category.sub_categories || !category.sub_categories.length) {
 						callback('All menu categories must have atleast one section');
 					} else {
-						async.each(description.sections, function(section) {
-							if (!section.section_name) {
+						async.each(category.sub_categories, function(sub_category) {
+							if (!sub_category.sub_category_name) {
 								callback('All sections must have a section name');
-							} else if (!section.items || !section.items.length) {
+							} else if (!sub_category.items || !sub_category.items.length) {
 								callback('All sections must have atleast one item');
 							} else {
 								callback();
@@ -358,10 +358,10 @@ angular.module('merchantApp').controller('MenuCreateController', ['$scope', 'mer
 				deferred.reject('Item required')
 			} else if (!$scope.current_item.item_tags) {
 				deferred.reject('Atleast one item tag is required');
-			} else if (!$scope.current_item.item_cost && $scope.current_item.item_options.length === 0) {
+			} else if (!$scope.current_item.item_cost && $scope.current_item.option_sets.length === 0) {
 				deferred.reject('Either item cost or atleast one item option mandatory');
 			} else {
-				async.each($scope.current_item.item_options, function(item_option, callback) {
+				async.each($scope.current_item.option_sets, function(item_option, callback) {
 					if(!item_option.option) {
 						callback('All item options must have a valid name');
 					} else if (!item_option.option_cost) {
