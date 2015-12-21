2ad90c1f03b0972a53bc258dec0b0f24b7d6e25c
Kuldeep Patel <kuldeepp89@gmail.com>
2015-12-21 13:48:34 +0530

    faye implemented and update in model

--- app_server.js
+++ app_server.js
@@ -6,6 +6,21 @@ var logger = require('tracer').colorConsole();
   var express = require('express');
   var app = express();
   var compression = require('compression');
+  var faye = require('faye');
+  var http = require('http');
+  var Bayeux = module.exports;
+
+  var bayeux = Bayeux.bayeux = new faye.NodeAdapter({
+    mount:    '/faye',
+    timeout:  100
+  });
+  bayeux.on('handshake', function(clientId) {
+    console.log( 'new client ' + clientId);
+  })
+  var server = http.createServer(app);
+
+  bayeux.attach(server);
+
 
   app.use(compression());
   // CONFIGURE THE APP & THE DATA MODELS
@@ -19,7 +34,7 @@ var logger = require('tracer').colorConsole();
   logger.info('Environment:' + settings.values.env);
   logger.info('URL:' + settings.values.config[settings.values.env].server);
   logger.info('Port:' + settings.values.config[settings.values.env].port);
-  app.listen(settings.values.config[settings.values.env].port);
+  server.listen(settings.values.config[settings.values.env].port);
   logger.info('Started the server');
   process.on('uncaughtException', function(error) {
     logger.info(error.stack);
