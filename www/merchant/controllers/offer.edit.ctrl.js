1cebf3172de360d5decc592d71de55b39727248f
Hemant Wadhwa <hemantwadhwa47@gmail.com>
2015-12-23 19:10:49 +0530

    merchant (offer panel) updates
    - [ ] item names displayed inline once item has been chosen
    - [ ] reward details generation update
    - [ ] add outlet issue fix

--- www/merchant/controllers/offer.edit.ctrl.js
+++ www/merchant/controllers/offer.edit.ctrl.js
@@ -172,26 +172,24 @@ angular.module('merchantApp')
                 console.log(newVal);
                 if (newVal.reward_type === 'buyxgety') {
                     delete newVal.item_free;
-                    if (newVal.item_x && newVal.item_y) {
+                    if (newVal.item_x) {
                         $scope.getRewardName(newVal.item_x).then(function(item_x) {
-                            $scope.getRewardName(newVal.item_y).then(function(item_y) {
-                                newVal.free_item_name = item_x;
-                                newVal.paid_item_name = item_y;
-                                $scope.offer.actions.reward.header = 'Buy ' + item_x;
-                                $scope.offer.actions.reward.line1 = 'Get ' + item_y;
-                                $scope.offer.actions.reward.line2 = '';
-                            }, function(err) {
-                                console.log(err);
-                                $scope.offer.actions.reward.header = 'testing';
-                                $scope.offer.actions.reward.line1 = 'testing';
-                                $scope.offer.actions.reward.line2 = 'testing';
-                            });
+                            newVal.free_item_name = item_x;
                         }, function(err) {
                             console.log(err);
-                            $scope.offer.actions.reward.header = 'testing';
-                            $scope.offer.actions.reward.line1 = 'testing';
-                            $scope.offer.actions.reward.line2 = 'testing';
-                        });
+                        });    
+                    }
+                    if (newVal.item_y) {
+                        $scope.getRewardName(newVal.item_y).then(function(item_y) {
+                            newVal.paid_item_name = item_y;
+                        }, function(err) {
+                            console.log(err);
+                        }); 
+                    }
+                    if (newVal.item_x && newVal.item_y) {
+                        $scope.offer.actions.reward.header = 'Buy ' + newVal.free_item_name;
+                        $scope.offer.actions.reward.line1 = 'Get ' + newVal.paid_item_name;
+                        $scope.offer.actions.reward.line2 = '';
                     } else {
                         $scope.offer.actions.reward.header = '';
                         $scope.offer.actions.reward.line1 = '';
@@ -291,7 +289,7 @@ angular.module('merchantApp')
                     $scope.cloneTimings({
                         _id: newOutlet
                     });
-                } else if ($scope.offer.offer_outlets.indexOf(newOutlet) !== -1) {
+                } else if ($scope.offer.offer_outlets.indexOf(newOutlet) === -1) {
                     $scope.offer.offer_outlets.push(newOutlet);
                 }
             };
