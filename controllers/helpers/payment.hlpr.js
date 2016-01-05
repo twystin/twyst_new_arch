56d594ab6e304e85608d35780d38293074fc53f4
Kuldeep Patel <kuldeepp89@gmail.com>
2016-01-05 19:00:56 +0530

    update in calculate checksum

--- controllers/helpers/payment.hlpr.js
+++ controllers/helpers/payment.hlpr.js
@@ -4,11 +4,16 @@ var crypto = require("crypto");
 var Q = require('q');
 var logger = require('tracer').colorConsole();
 
-module.exports.calculate_checksum = function(message) {
+module.exports.calculate_checksum = function(message, type) {
 	logger.log();
 	var deferred = Q.defer();
-
-	var key = 'be92fd65d03d43bc83a5aaeffdcd709f';
+	if(type === 'wallet') {
+		var key = 'gbzbj7859G6STy3zyzL4gj4AEuaF';
+	}
+	else{
+		var key = 'be92fd65d03d43bc83a5aaeffdcd709f';	
+	}
+	
 	var checksum = crypto.createHmac('sha256', key).update(message).digest('hex')
 	data.calculated_checksum = checksum;
 	deferred.resolve(data);
--- controllers/payment.ctrl.js
+++ controllers/payment.ctrl.js
@@ -4,6 +4,7 @@ var logger = require('tracer').colorConsole();
 var PaymentHelper = require('./helpers/payment.hlpr.js');
 var HttpHelper = require('../common/http.hlpr.js');
 var _ = require('underscore');
+var xml = require('xml');
 
 module.exports.get_zaakpay_response = function(req, res) {
 	logger.log();
@@ -24,37 +25,28 @@ module.exports.calculate_checksum = function(req, res) {
 	var order_form = {};
 	order_form = _.extend(order_form, req.body);
 	console.log(order_form);
-	var merchantIdentifier = req.body.merchantIdentifier;
-	var orderId = req.body.orderId;
-	var returnUrl = 'http://staging.twyst.in/api/v4/zaakpay_response';
-	var buyerEmail = req.body.buyerEmail;
-	var buyerFirstName = req.body.buyerFirstName;
-	var buyerLastName = req.body.buyerLastName;
-	var buyerAddress = req.body.buyerAddress;
-	var buyerCity = req.body.buyerCity;
-	var buyerState = req.body.buyerState;
-	var buyerCountry = req.body.buyerCountry;
-	var buyerPincode = req.body.buyerPincode;
-	var buyerPhoneNumber = req.body.buyerPhoneNumber;
-	var txnType = req.body.txnType;
-	var zpPayOption = req.body.zpPayOption;
-	var mode = req.body.mode;
-	var currency = req.body.currency;
-	var amount = req.body.amount;
-	var merchantIpAddress = req.body.merchantIpAddress;
-	var txnDate = req.body.txnDate;
-	var purpose = req.body.purpose;
-	var productDescription = req.body.productDescription;
+	var message;
+	if(order_form.pgName === 'wallet') {
+		var mid = order_form.mid;
+		var amount = order_form.amount;
+		var orderId = order_form.orderd;
+		var message = "'"+mid+"''"+amount+"''"+orderd+"'";		
+	}
+	else if(order_form.pgName === 'zaakpay') {
+		var message = 'test'
+	}
+	
 
-	var message = "'"+merchantIdentifier+"''"+orderId+"''"+returnUrl+"''"+buyerEmail
-	+"''"+buyerFirstName+"''"+buyerLastName+"''"+buyerAddress+"''"+buyerCity
-	+"''"+buyerState+"''"+buyerCountry+"''"+buyerPincode+"''"+buyerPhoneNumber+"''"+txnType
-	+"''"+zpPayOption+"''"+mode+"''"+currency+"''"+amount+"''"+merchantIpAddress+"''"+txnDate
-	+"''"+purpose+"''"+productDescription+"'";
+	
 	console.log(message);
 
-	PaymentHelper.calculate_checksum(message).then(function(data){
-		HttpHelper.success(res, data );
+	PaymentHelper.calculate_checksum(message, order_form.pgName).then(function(data){
+		var calculated_checksum = data.calculated_checksum;
+		var checksum = {};
+		checksum.status  = 'SUCCESS';
+		checksum.checksumValue = calculated_checksum;
+			
+		HttpHelper.success(res, xml(checksum) );
 	},	function(err) {
 		HttpHelper.error(res, err );
   	});
