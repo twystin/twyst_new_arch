0c911e9bafb4b84169ad6e24690618d4bebbc4c2
Kuldeep Patel <kuldeepp89@gmail.com>
2016-01-21 12:16:57 +0530

    removing www folder

--- www/console/directives/uiFileUpload.js
+++
@@ -1,8 +0,0 @@
-angular.module('consoleApp').directive('uiFileUpload', function() {
-    return {
-        restrict: 'A',
-        link: function(scope, ele) {
-            ele.bootstrapFileInput();
-        }
-    }
-});
