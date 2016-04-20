1aea807cab8af3532d6db566fdc57bd21ec8e7e5
Kuldeep Patel <kuldeepp89@gmail.com>
2016-04-20 20:57:55 +0530

    some fixes

--- models/banner.mdl.js
+++ models/banner.mdl.js
@@ -8,31 +8,20 @@ var hours = require("./partials/hours.mdl");
 var Outlet = mongoose.model('Outlet');
 
 var Banner = new Schema({
-	banner_type: {type: String}, //third_party_banner,food_banner,landing_page_banner,outlet_banner
+	banner_type: {type: String}, //third_party,single_outlet,landing_page,multi_outlets
 	banner_name: {type: String},
 	outlets: [{
         type: Schema.ObjectId,
         ref: 'Outlet'
     }],
 	banner_image: {type: String, default: ''},
-	cost: {type: Number, default: 0}, // cost in twyst cash
 	coupon_code: String,
-	max_use: {type: Number, default: 1},	
 	end_date: Date,
 	header: {
 	  type: String
 	},
-	line1: {
-	  type: String
-	},
-	line2: {
-	  type: String
-	},
 	description: {
-	  type: String
-	},
-	terms: {
-	  type: String
+		type: String
 	}
 });
 
